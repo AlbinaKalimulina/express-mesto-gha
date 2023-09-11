f32132d078e63860543d9904decd89622788c320
Albina Kalimulina <albinashamig@yandex.ru>
2023-09-11 05:27:21 +0500

    ПР13

--- controllers/users.js
+++ controllers/users.js
@@ -2,11 +2,15 @@ const User = require('../models/user');
 
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
-
   User.create({ name, about, avatar })
-    // .then((user) => res.status(201).send(user))
     .then((user) => res.send({ data: user }))
-    .catch(() => res.status(500).send({ message: 'На сервере произошла ошибка' }));
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: 'Некорректные данные' });
+      } else {
+        res.status(500).send({ message: 'На сервере произошла ошибка' });
+      }
+    });
 };
 
 module.exports.getUsers = (req, res) => {
