9107ac3344084706af9be739fd360fe58dd4ee72
Albina Kalimulina <albinashamig@yandex.ru>
2023-09-26 20:55:05 +0500

    ПР14

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const {
   getUsersMe, getUsers, getUserById, editUserProfile, editUserAvatar,
 } = require('../controllers/users');
+
+const urlRegex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/;
 
 // возвращает информацию о текущем пользователе
 router.get('/me', getUsersMe);
@@ -9,10 +12,27 @@ router.get('/me', getUsersMe);
 router.get('/', getUsers);
 
 // возвращает пользователя по _id
-router.get('/:userId', getUserById);
+router.get('/:userId', celebrate({
+  params: Joi.object().keys({
+    id: Joi.string().length(24).hex().required(),
+  }),
+}), getUserById);
+
 // обновляет профиль
-router.patch('/me', editUserProfile);
+router.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+  }),
+}), editUserProfile);
+
 // обновляет аватар
-router.patch('/me/avatar', editUserAvatar);
+router.patch('/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi
+      .string()
+      .pattern(urlRegex),
+  }),
+}), editUserAvatar);
 
 module.exports = router;
